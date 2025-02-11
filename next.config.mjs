135d6b4228ff8c20ab903bfb3d03ab78c85a724a
Sushant Bhat <sush1393@gmail.com>
2025-02-11 02:46:09 -0500

    fixes for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,15 @@
 /** @type {import('next').NextConfig} */
-const moduleExports = {
+const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   reactStrictMode: true,
   swcMinify: true,
-  output: "export",
-  trailingSlash: true,
+  output: "export", // Ensure static export is enabled
+  trailingSlash: true, // Fixes navigation issues for static export
   webpack: config => {
-    /* ... */
-  },
-  sentry: {
-    /* ... */
-  },
-  env: {
-    /* ... */
+    return config;
   },
   transpilePackages: ["@codegouvfr/react-dsfr", "tss-react"]
 };
-export default {
-  /* ... */
-};
+
+export default nextConfig;
